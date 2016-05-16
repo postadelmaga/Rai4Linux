2b18ecd34c5fe9971345477c372d89b1cb9b8f2e
Francesco <postadelmaga@gmail.com>
2016-05-16 17:29:11 +0800

    remove chat

--- blocks/chat.php
+++ blocks/chat.php
@@ -1,15 +1,3 @@
 <?php if ($_SERVER['HTTP_HOST'] != 'localhost'): ?>
-    <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
-        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-        (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/573990322a5b82c55a4fdae6/default';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-        })();
-    </script>
-    <!--End of Tawk.to Script-->
+
 <?php endif; ?>
