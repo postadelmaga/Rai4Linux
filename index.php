9967925be0ce20d9d6b610cd433bc477d38e0e04
Francesco <postadelmaga@gmail.com>
2018-05-13 13:33:55 +0800

    new start

--- index.php
+++ index.php
@@ -81,10 +81,8 @@ umask(0);
  * the cache_id ( see BurnOut_PageCache_Model_Processor::prepareCacheId() )
  */
 
-//$mageRunCode = isset($_SERVER['MAGE_RUN_CODE']) ? $_SERVER['MAGE_RUN_CODE'] : '';
-$mageRunCode = "en";
+$mageRunCode = isset($_SERVER['MAGE_RUN_CODE']) ? $_SERVER['MAGE_RUN_CODE'] : '';
 /* Run store or run website */
-//$mageRunType = isset($_SERVER['MAGE_RUN_TYPE']) ? $_SERVER['MAGE_RUN_TYPE'] : 'store';
-$mageRunType = "store";
+$mageRunType = isset($_SERVER['MAGE_RUN_TYPE']) ? $_SERVER['MAGE_RUN_TYPE'] : 'store';
 
 Mage::run($mageRunCode, $mageRunType);
